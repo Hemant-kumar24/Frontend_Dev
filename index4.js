ba6f625e3f767797e75fcc7496c71fd5aaffc531
Hemant-kumar24 <hemantkushwaha2405@gmail.com>
2025-11-19 08:51:40 +0530

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,21 @@
+greeting = "Welcome"; 
+
+"use strict";
+
+function showMessage() {
+  let greeting = "Welcome"; 
+  console.log(greeting);
+}
+
+showMessage();
+"use strict";
+
+function showMessage() {
+  let greeting = "Welcome";
+
+  debugger; 
+
+  console.log(greeting);
+}
+
+showMessage();
